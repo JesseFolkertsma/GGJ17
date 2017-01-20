b35ea4655d74fa7154ee375521d498340df40175
Lennart Neelis <Lennart_neelis@hotmail.com>
2017-01-20 22:43:26 +0100

    movement interface

---
+++ Unity/GGJ17/Assets/GGJ17/Scripts/Player/IMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Corn.Movement
+{
+    public interface IMovement
+    {
+        void Move (Vector2 dir_);
+
+        void Rotate (float rot_);
+
+        void Run (bool run_);
+
+        void Melee ();
+    }
+}
