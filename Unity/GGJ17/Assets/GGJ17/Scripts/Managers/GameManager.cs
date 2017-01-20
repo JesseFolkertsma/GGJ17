a4a90ff20de3cb31051febdd8d766f2687fd249d
JesseFolkertsma <jesse.folkertsma@gmail.com>
2017-01-20 22:43:16 +0100

    menuworking

--- Unity/GGJ17/Assets/GGJ17/Scripts/Managers/GameManager.cs
+++ Unity/GGJ17/Assets/GGJ17/Scripts/Managers/GameManager.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
     public static GameManager instance;
+    public GameObject pauzecanvas;
+    public bool inArena = false;
+    bool gamePauzed = false;
 
     #region GameStats
     [Header ("GameStats")]
@@ -16,6 +20,7 @@ public class GameManager : MonoBehaviour {
         if(instance == null)
         {
             instance = this;
+            DontDestroyOnLoad(gameObject);
         }
         else
         {
@@ -23,18 +28,68 @@ public class GameManager : MonoBehaviour {
         }
     }
 
-    public void PauzeGame()
+    void OnLevelWasLoaded()
     {
-
+        if (inArena)
+        {
+            if (pauzecanvas == null)
+            {
+                pauzecanvas = FindObjectOfType<PauzeMenu>().gameObject;
+                pauzecanvas.SetActive(false);
+            }
+        }
     }
 
-    public void StartGame()
+    void Update()
     {
+        if (inArena)
+        {
+            if (Input.GetButtonDown("Cancel"))
+            {
+                if (!gamePauzed)
+                {
+                    PauzeGame();
+                }
+                else
+                {
+                    ContinueGame();
+                }
+            }
+        }
+    }
 
+    public void PauzeGame()
+    {
+        Debug.Log("Game pauzed");
+        gamePauzed = true;
+        Time.timeScale = 0;
+        pauzecanvas.SetActive(true);
+    }
+
+    public void ContinueGame()
+    {
+        Debug.Log("Continueing game");
+        gamePauzed = false;
+        Time.timeScale = 1;
+        pauzecanvas.SetActive(false);
+    }
+
+    public void StartGame(int buildI = 1)
+    {
+        Debug.Log("Starting Arena " + buildI);
+        inArena = true;
+        SceneManager.LoadScene(buildI);
     }
 
     public void EndGame()
     {
+        Debug.Log("Ending game");
+    }
 
+    public void ReturnToMenu()
+    {
+        Debug.Log("Returning to menu");
+        inArena = false;
+        SceneManager.LoadScene(0);
     }
 }
---
+++ Unity/GGJ17/Assets/GGJ17/Scripts/UI/PauzeMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauzeMenu : MonoBehaviour {
+
+    public void ExitToMenu()
+    {
+        GameManager.instance.ReturnToMenu();
+    }
+
+    public void Restart()
+    {
+        GameManager.instance.StartGame();
+    }
+}
