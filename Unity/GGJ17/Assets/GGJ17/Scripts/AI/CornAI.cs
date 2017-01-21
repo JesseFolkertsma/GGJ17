f390f930359ff1c62da3f9730390a6b97ff92803
Lennart Neelis <Lennart_neelis@hotmail.com>
2017-01-21 12:33:50 +0100

    corn ai

---
+++ Unity/GGJ17/Assets/GGJ17/Scripts/AI/CornAI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CornAI : MonoBehaviour {
+
+    public Transform[] kernelsLocation;
+
+    // Use this for initialization
+    void Start ()
+    {
+        for (int i = 0; i < kernelsLocation.Length; i++)
+        {
+            Kernel sock = kernelsLocation[i].gameObject.AddComponent<Kernel>();
+            sock.Heal(0);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
