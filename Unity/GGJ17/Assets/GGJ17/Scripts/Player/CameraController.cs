2be3843316a0100b2c9a5a8c54399dc735f9130e
JesseFolkertsma <jesse.folkertsma@gmail.com>
2017-01-21 03:49:59 +0100

    end_of_day

---
+++ Unity/GGJ17/Assets/GGJ17/Scripts/Player/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    [Header("Public variables")]
+    public bool isZoomed = false;
+    public Vector3 offset = new Vector3(0, 2, -2);
+    public Vector3 zoomOffset = new Vector3(1, 1.5f, -1f);
+    public float followSpeed = 5f;
+        
+    [SerializeField]
+    Transform camPosition;
+
+    void Update()
+    {
+        Vector3 _offset = Vector3.zero;
+        if (!isZoomed)
+        {
+            _offset = offset;
+        }
+        else
+        {
+            _offset = zoomOffset;
+        }
+        camPosition.localPosition = offset;
+
+        //transform.position = Vector3.Lerp(transform.position, camPosition.position, Time.deltaTime * followSpeed);
+        transform.position = camPosition.position;
+        transform.rotation = camPosition.rotation;
+    }
+}
