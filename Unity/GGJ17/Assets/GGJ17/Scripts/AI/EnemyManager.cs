5dbf2119a66156ea204180d68206be23a4838e17
Lennart Neelis <Lennart_neelis@hotmail.com>
2017-01-22 01:17:22 +0100

    ai update

---
+++ Unity/GGJ17/Assets/GGJ17/Scripts/AI/EnemyManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour {
+
+    public static EnemyManager instance;
+
+    public GameObject[] Enemys;
+
+    public GameObject AquireTarget (GameObject self) {
+
+        float dist = 9999999;
+        GameObject closest = null;
+        for (int i = 0; i < Enemys.Length; i++)
+        {
+            if (Enemys[i] != self)
+            {
+                float myDist = Vector3.Distance(Enemys[i].transform.position, self.transform.position);
+                if (myDist < dist)
+                {
+                    dist = myDist;
+                    closest = Enemys[i];
+                }
+            }
+        }
+        return closest;
+    }
+
+
+	void Awake ()
+    {
+        instance = this;
+
+	}
+
+    void Update ()
+    {
+
+
+    }
+}
