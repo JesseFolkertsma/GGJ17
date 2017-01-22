c1fcf0d88b08c0bd3800156334985c5cb74ef363
Jochem Van Diepen <jochemiscool@hotmail.com>
2017-01-22 13:37:10 +0100

    TERING!

---
+++ Unity/GGJ17/Assets/GGJ17/Scripts/InstantiateOnVictory.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+var popCorn : GameObject;
+var tering : GameObject;
+
+function Start () {
+	
+}
+
+function FixedUpdate () {
+    Instantiate(popCorn, gameObject.transform.position, Quaternion.identity);
+    transform.Rotate(Vector3.up, 30 * Time.deltaTime);
+}
+
+function TeringHond () {
+    tering.SetActive(true);
+
+}
